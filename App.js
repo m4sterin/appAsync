d249b2fc92fd0de25a8402e8c7adcca8eb0c5b61
Caio Rodrigues <caiorodrigues1@unipam.edu.br>
2021-04-15 21:35:16 -0300

    Ajustes Para Atender a aps

--- App.js
+++ App.js
@@ -1,58 +1,34 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard } from 'react-native';
-import AsyncStorage from '@react-native-community/async-storage'
-
+import { StyleSheet, View, Text, FlatList} from 'react-native';
+import api from './src/services/api'
+import Produtos from './src/components/Produtos'
 export default class App extends Component {
+
   constructor(props){
     super(props);
     this.state = { 
-      input: '',
-      nome: ''
-    };
-
-    this.gravaNome = this.gravaNome.bind(this);
-  }
-
-  async componentDidMount() {
-    await AsyncStorage.getItem('nome').then((value) => {
-      this.setState({nome: value})
-    })
-  }
-
-  async componentDidUpdate(_, prevState) {
-    const nome = this.state.nome;
-    if(prevState != nome) {
-      await AsyncStorage.setItem('nome', nome)
+     produtos: []
     }
   }
 
-  gravaNome() {
+  async componentDidMount() {
+    const response = await api.get('items');
     this.setState({
-     nome: this.state.input 
+      produtos: response.data
     })
-    alert('Salvo com sucesso!');
-    Keyboard.dismiss();
   }
 
-  render(){
+  render() {
   return (
     <View style={styles.container}>
-      <View style={styles.inputView}>
-        <TextInput
-        style={styles.input}
-        value={this.state.input}
-        onChangeText={(text) => this.setState({ input: text})}
-        underlineColorAndroid='transparent'
-        />
-        <TouchableOpacity onPress={this.gravaNome}>
-          <Text style={styles.btnAcess}>+</Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <Text style={styles.name}>{this.state.nome}</Text>
-      </View>
+      <Text style={styles.title}>Supermarket WOW</Text>
+      <FlatList
+        data={this.state.produtos}
+        keyExtractor={item => item.id.toString()}
+        renderItem={ ({item}) => <Produtos data={item} />}
+      />
     </View>
-  );
+    );
   }
 }
 
@@ -60,30 +36,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    marginTop: 20,
+    justifyContent: 'center',
+    backgroundColor: '#DDD',
   },
-  inputView: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  input: {
-    width: 350,
-    height: 40,
-    borderColor: '#000',
-    borderWidth: 1,
-    padding: 10,
-    margin: 10,
-  },
-  btnAcess: {
-    backgroundColor: '#222',
-    color: '#FFF',
-    height: 40,
-    padding: 10,
-    marginLeft: 4,
-  },
-  name: {
-    marginTop: 15,
-    fontSize: 30,
-    textAlign: 'center',
+  title: {
+    fontSize: 32,
+    color: "#FFFF",
+    fontWeight: 600,
   }
 });
